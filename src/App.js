00ad16ffadd30faa74d9b35602b56ecfde8f2185
Magno Valdetaro <mvaldetaro@gmail.com>
2021-12-13 11:25:49 -0300

    Limpeza home

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { Basic, Button } from "investira.react.components";
-import { renders, displays } from "investira.react.lib";
+// import { Highlight } from "investira.react.components";
+// import { displays } from "investira.react.lib";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Basic>Teste</Basic>
-        <Button variant="outlined" color="primary">
-          Teste
-        </Button>
-        {displays.cpf("10403782775")}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"> Test Zone</div>;
 }
 
 export default App;
