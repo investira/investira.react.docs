2f723cdc06e1b0a30142b69b4e5e68c3ebf082a4
willlucas1512 <willlucas1512@gmail.com>
2021-12-22 16:48:24 -0300

    Update Info.stories.js

--- src/stories/Info.stories.js
+++ src/stories/Info.stories.js
@@ -1,12 +1,5 @@
 import React from "react";
 import { Info } from "investira.react.components";
-import {
-  withKnobs,
-  text,
-  radios,
-  boolean,
-  select,
-} from "@storybook/addon-knobs";
 import { variants, textColors } from "./options";
 
 export default {
@@ -15,41 +8,61 @@ export default {
     componentSubtitle:
       "Display de informações de label e valor com layout personalizável",
   },
-  decorators: [withKnobs],
   component: Info,
+  argTypes: {
+    variant: {
+      options: variants,
+      control: { type: "select" },
+    },
+    variantValue: {
+      options: variants,
+      control: { type: "select" },
+    },
+    valueColor: {
+      options: textColors,
+      control: { type: "radios" },
+    },
+    labelColor: {
+      options: textColors,
+      control: { type: "radios" },
+    },
+    gutter: {
+      options: {
+        left: "left",
+        full: "full",
+        right: "right",
+      },
+      control: { type: "radios" },
+    },
+
+    direction: {
+      options: {
+        vertical: "vertical",
+        horizontal: "horizontal",
+      },
+      control: { type: "radios" },
+    },
+  },
 };
 
-export const Default = () => {
-  return (
-    <div
-      style={{
-        backgroundColor: "#191b2a",
-        padding: "16px",
-        borderRadius: "10px",
-        width: "300px",
-      }}
-    >
-      <Info
-        gutter={radios("Gutter", {
-          left: "left",
-          full: "full",
-          right: "right",
-        })}
-        gutterBottom={boolean("gutterBottom", false)}
-        colon={boolean("Dois pontos", false)}
-        bold={boolean("Valor em negrito", false)}
-        direction={radios(
-          "Direção",
-          { vertical: "vertical", horizontal: "horizontal" },
-          "vertical"
-        )}
-        variant={select("Tamanho da fonte da label", variants, "caption")}
-        variantValue={select("Tamanho da fonte do valor", variants, "caption")}
-        valueColor={radios("Cor do valor", textColors, "textSecondary")}
-        labelColor={radios("Cor da label", textColors, "textSecondary")}
-        value={text("Valor", "Valor")}
-        label={text("Legenda", "Legenda")}
-      />
-    </div>
-  );
+const Template = (args) => (
+  <div
+    style={{
+      backgroundColor: "#191b2a",
+      padding: "16px",
+      borderRadius: "10px",
+      width: "300px",
+    }}
+  >
+    <Info />
+  </div>
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  value: "R$ 1.000,00",
+  label: "Teste",
+  colon: false,
+  bold: false,
+  gutterBottom: false,
 };
