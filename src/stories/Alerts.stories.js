4c51a28fbaab5c82b4362491e05b08f467550170
willlucas1512 <willlucas1512@gmail.com>
2022-01-06 16:00:45 -0300

    Update Alerts.stories.js

--- src/stories/Alerts.stories.js
+++ src/stories/Alerts.stories.js
@@ -1,12 +1,5 @@
 import React from "react";
 import { Alerts } from "investira.react.components";
-import {
-  withKnobs,
-  text,
-  boolean,
-  select,
-  radios,
-} from "@storybook/addon-knobs";
 import { alertsColors, textColors, icons } from "./options";
 
 export default {
@@ -14,20 +7,28 @@ export default {
   parameters: {
     componentSubtitle: "Alertas",
   },
-  decorators: [withKnobs],
   component: Alerts,
+  argTypes: {
+    iconName: {
+      options: icons,
+      control: { type: "select" },
+    },
+    backgroundColor: {
+      options: alertsColors,
+      control: { type: "select" },
+    },
+    color: {
+      options: textColors,
+      control: { type: "radio" },
+    },
+  },
 };
 
-export const Default = () => {
-  return (
-    <Alerts
-      close={boolean("close", true)}
-      iconName={select("Ícone", icons, "callcenter")}
-      open={boolean("open", true)}
-      backgroundColor={select("Cor de fundo", alertsColors, "default")}
-      color={radios("Cor de texto", textColors)}
-    >
-      {text("Conteúdo", "Este é um alerta.")}{" "}
-    </Alerts>
-  );
+export const Primary = (args) => <Alerts {...args}>{args.text}</Alerts>;
+Primary.args = {
+  close: true,
+  iconName: "callcenter",
+  open: true,
+  backgroundColor: "default",
+  text: "Este é um alerta.",
 };
