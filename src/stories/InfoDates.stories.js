936f331b6dd53937136c444b5560a0f3f12786fc
willlucas1512 <willlucas1512@gmail.com>
2022-01-06 15:41:45 -0300

    Update InfoDates.stories.js

--- src/stories/InfoDates.stories.js
+++ src/stories/InfoDates.stories.js
@@ -1,49 +1,63 @@
 import React from "react";
 import { InfoDates } from "investira.react.components";
-import { boolean, date, select, text, withKnobs } from "@storybook/addon-knobs";
 import { textColors, variants } from "./options";
+
+const optionsFormat = {
+  custom: "custom",
+  datetime: "datetime",
+  date: "date",
+  duration: "duration",
+  fromnow: "fromnow",
+};
 
 export default {
   title: "InfoDates",
   parameters: {
     componentSubtitle: "Display de informações de data",
   },
-  decorators: [withKnobs],
   component: InfoDates,
+  argTypes: {
+    color: {
+      options: textColors,
+      control: { type: "select" },
+    },
+    direction: {
+      options: {
+        horizontal: "horizontal",
+        vertical: "vertical",
+      },
+      control: { type: "radio" },
+    },
+    format: {
+      control: { type: "text" },
+    },
+    time: {
+      control: { type: "date" },
+    },
+    variant: {
+      options: optionsFormat,
+      control: { type: "select" },
+    },
+    labelVariant: {
+      options: variants,
+      control: { type: "select" },
+    },
+    timeVariant: {
+      options: variants,
+      control: { type: "select" },
+    },
+  },
 };
 
-export const Default = () => {
-  const optionsFormat = {
-    custom: "custom",
-    datetime: "datetime",
-    date: "date",
-    duration: "duration",
-    fromnow: "fromnow",
-  };
-  return (
-    <div
-      style={{
-        backgroundColor: "#191b2a",
-        padding: "16px",
-        borderRadius: "10px",
-      }}
-    >
-      <InfoDates
-        color={select("Cor da data", textColors, "textSecondary")}
-        direction={select("Direção", {
-          horizontal: "horizontal",
-          vertical: "vertical",
-        })}
-        bold={boolean("Negrito", false)}
-        format={text("Formato (moment.js)")}
-        colon={(boolean("Dois pontos"), false)}
-        gutterBottom={boolean("gutterBottom", true)}
-        label={text("Label", "Label")}
-        time={date("Data")}
-        variant={select("Formatação", optionsFormat, "datetime")}
-        labelVariant={select("Tamanho da fonte da label", variants, "caption")}
-        timeVariant={select("Tamanho da fonte da data", variants, "caption")}
-      />
-    </div>
-  );
+export const Primary = (args) => <InfoDates {...args} />;
+
+Primary.args = {
+  color: "textSecondary",
+  bold: false,
+  colon: false,
+  gutterBottom: true,
+  label: "Label",
+  variant: "datetime",
+  labelVariant: "caption",
+  timeVariant: "caption",
 };
