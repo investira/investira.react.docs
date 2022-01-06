64838233e8202ce8cf7e9f4dced6f3c800057b4c
willlucas1512 <willlucas1512@gmail.com>
2022-01-06 11:23:37 -0300

    add icons, css e backgrounds

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,16 +1,38 @@
 import React from "react";
 import { ThemeProvider } from "investira.react.components";
 import { themePrimary } from "../src/styles/invThemes";
+import { ReactComponent as IconSprite } from "../src/images/symbol-defs.svg";
+import "../src/global/global.css";
 
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={themePrimary}>
-      <Story />
+      <div id={"test-root"} className={"themePrimary"}>
+        <Story />
+        <IconSprite />
+      </div>
     </ThemeProvider>
   ),
 ];
 
 export const parameters = {
+  backgrounds: {
+    default: "Background Dark",
+    values: [
+      {
+        name: "Primary",
+        value: "#00dfa8",
+      },
+      {
+        name: "Background Dark",
+        value: "#191b2a",
+      },
+      {
+        name: "Secondary",
+        value: "#2e2e46",
+      },
+    ],
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
