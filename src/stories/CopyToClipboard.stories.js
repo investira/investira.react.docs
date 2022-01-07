43bca64bf5e70967f534c7a739fe98b5bf85caa8
willlucas1512 <willlucas1512@gmail.com>
2022-01-07 11:11:33 -0300

    Update CopyToClipboard.stories.js

--- src/stories/CopyToClipboard.stories.js
+++ src/stories/CopyToClipboard.stories.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { text, withKnobs } from "@storybook/addon-knobs";
 import { CopyToClipboard } from "investira.react.components";
 
 export default {
   title: "CopyToClipboard",
   component: CopyToClipboard,
-  decorators: [withKnobs],
 };
 
-export const Default = () => {
-  return <CopyToClipboard value={text("Texto a copiar", "Exemplo")} />;
+export const Default = (args) => {
+  return <CopyToClipboard {...args} />;
+};
+Default.args = {
+  value: "Exemplo",
 };
