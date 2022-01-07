c53617edd6209a971ecfb00bb334e5cdf1ab00df
willlucas1512 <willlucas1512@gmail.com>
2022-01-07 14:29:25 -0300

    Update FilterBar.stories.js

--- src/stories/FilterBar.stories.js
+++ src/stories/FilterBar.stories.js
@@ -1,28 +1,45 @@
 import React from "react";
-import { withKnobs, select, boolean } from "@storybook/addon-knobs";
 import { FilterBar } from "investira.react.components";
 import { iconColors, icons } from "./options";
 
 export default {
   title: "FilterBar",
   component: FilterBar,
-  decorators: [withKnobs],
+  argTypes: {
+    iconName: {
+      options: icons,
+      control: { type: "select" },
+    },
+    color: {
+      options: iconColors,
+      control: { type: "select" },
+    },
+    default: {
+      options: iconColors,
+      control: { type: "select" },
+    },
+  },
 };
 
-const generateData = (size = 6) => {
-  let xData = [];
+export const Default = (args) => {
+  const generateData = (size = 6) => {
+    let xData = [];
 
-  for (let xI = 0; xI <= size; xI++) {
-    xData.push({
-      label: `FILTER ${xI}`,
-      iconName: select("Ícone", icons, "ok"),
-      color: select("Cor do ícone selecionado", iconColors, "primary"),
-      default: select("Cor do ícone não selecionado", iconColors, "secondary"),
-    });
-  }
-  return xData;
+    for (let xI = 0; xI <= size; xI++) {
+      xData.push({
+        label: `FILTER ${xI}`,
+        iconName: args.iconName,
+        color: args.color,
+        default: args.default,
+      });
+    }
+    return xData;
+  };
+  return <FilterBar {...args} data={generateData()} />;
 };
-
-export const Default = () => {
-  return <FilterBar margin={boolean("Margem", true)} data={generateData()} />;
+Default.args = {
+  margin: true,
+  iconName: "ok",
+  color: "primary",
+  default: "secondary",
 };
