6e2607dbf035238bca847af3a1310b82f508bd14
willlucas1512 <willlucas1512@gmail.com>
2022-01-24 14:33:12 -0300

    Update MaskedTextField.stories.js

--- src/stories/MaskedTextField.stories.js
+++ src/stories/MaskedTextField.stories.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { withKnobs } from "@storybook/addon-knobs";
 import { MaskedTextField } from "investira.react.components";
 
 export default {
   title: "MaskedTextField",
   component: MaskedTextField,
-  decorators: [withKnobs],
 };
 
 export const Default = () => {
