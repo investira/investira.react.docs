38d5d698d1b66c06d0bafbe66f3be923f9ec8548
willlucas1512 <willlucas1512@gmail.com>
2022-01-25 10:20:23 -0300

    Update ProgressBar.stories.js

--- src/stories/ProgressBar.stories.js
+++ src/stories/ProgressBar.stories.js
@@ -1,26 +1,28 @@
 import React from "react";
 import { ProgressBar } from "investira.react.components";
-import { withKnobs, number, radios } from "@storybook/addon-knobs";
-import { barRange, basicColors } from "./options";
+import { basicColors } from "./options";
 
 export default {
   title: "ProgressBar",
   parameters: {
     componentSubtitle: "ProgressBar",
   },
-  decorators: [withKnobs],
   component: ProgressBar,
+  argTypes: {
+    color: {
+      options: basicColors,
+      control: { type: "radio" },
+    },
+    animate: {
+      options: ["indeterminate", "progress"],
+      control: { type: "radio" },
+    },
+  },
 };
 
-export const Default = () => {
-  return (
-    <ProgressBar
-      value={number("Valor", 100, barRange)}
-      color={radios("Cor", basicColors)}
-      animate={radios("Animação", {
-        Indeterminada: "indeterminate",
-        Progresso: "progress",
-      })}
-    />
-  );
+export const Default = (args) => {
+  return <ProgressBar {...args} />;
+};
+Default.args = {
+  value: 100,
 };
