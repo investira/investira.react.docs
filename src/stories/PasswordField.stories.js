9a0132c27e8de76019b9e91443813108707e969c
willlucas1512 <willlucas1512@gmail.com>
2022-01-25 10:20:28 -0300

    Update PasswordField.stories.js

--- src/stories/PasswordField.stories.js
+++ src/stories/PasswordField.stories.js
@@ -1,24 +1,11 @@
 import React from "react";
-import { withKnobs } from "@storybook/addon-knobs";
 import { PasswordField } from "investira.react.components";
 
 export default {
   title: "PasswordField",
   component: PasswordField,
-  decorators: [withKnobs],
 };
 
 export const Default = () => {
-  return (
-    <div
-      style={{
-        backgroundColor: "#191b2a",
-        padding: "16px",
-        width: "239px",
-        borderRadius: "10px",
-      }}
-    >
-      <PasswordField />
-    </div>
-  );
+  return <PasswordField />;
 };
